872a4d2c8f856c39d1d25de3eddc85d600b5ebb4
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2026-01-05 10:03:11 +0500

    save

--- src/widgets/aboutSummit/ui/AboutContent.tsx
+++ src/widgets/aboutSummit/ui/AboutContent.tsx
@@ -89,99 +89,28 @@ export function AboutContent() {
 				</Container>
 			</section>
 
-			<section className="relative bg-white pt-[100px]">
+			<section className="relative bg-white pt-[50px]">
 				<Container className="relative z-10 flex items-start justify-between">
-					<div>
+					<div className="-translate-y-[60px] transform">
 						<Image
 							src="/imgs/about-page-img-3.png"
 							alt="About Summit"
-							width={509}
-							height={662}
-							className="block max-w-[509px] rounded-[12px]"
+							width={874}
+							height={1070}
+							className="block max-w-[874px] rounded-[12px]"
 						/>
 					</div>
-					<div className="flex items-start justify-between gap-[30px]">
-						<div
-							ref={TextRef}
-							className="text-text relative mb-[60px] max-w-[858px] grow translate-y-[50px] text-[24px] leading-normal opacity-0"
+					<div className="max-w-[869px]">
+						<h2
+							ref={PresidentInitiativeTitleRef}
+							className="text-text mb-[60px] translate-y-[50px] text-[48px] leading-[1.2] font-bold opacity-0"
 						>
-							{t('pages.about.text2')}
-						</div>
-					</div>
+							{t('pages.about.presidentInitiative')}
+						</h2>
+						<p className="mb-[60px]">{t('pages.about.aboutText4')}</p>
 
-					<div className="relative z-1 mb-[15px] flex items-start justify-between gap-[30px]">
-						<div className="max-w-[721px]">
-							<h2
-								ref={PresidentInitiativeTitleRef}
-								className="text-text mb-[60px] translate-y-[50px] text-[48px] leading-[1.2] font-bold opacity-0"
-							>
-								{t('pages.about.presidentInitiative')}
-							</h2>
-
-							<p
-								ref={PresidentInitiativeTextRef}
-								className="text-text mb-[60px] translate-y-[50px] text-[16px] leading-normal font-normal whitespace-pre-line opacity-0"
-							>
-								{t('pages.about.presidentInitiativeText')}
-							</p>
-
-							<div
-								ref={QuoteRef}
-								className="relative translate-y-[50px] pt-[39px] pl-[71px] opacity-0"
-							>
-								<div className="text-muted absolute top-0 left-0 text-[128px] leading-none">”</div>
-								<div className="font-regular mb-[50px] max-w-[493px] text-[16px] leading-normal whitespace-pre-line text-black">
-									{t('pages.about.presidentText')}
-								</div>
-
-								<div
-									ref={PresidentInfoRef}
-									className="flex translate-y-[50px] items-center gap-[30px] opacity-0"
-								>
-									<Image
-										src="/imgs/president.png"
-										alt="About President"
-										width={102}
-										height={102}
-										className="block max-w-[102px] rounded-full"
-									/>
-									<div className="text-primary max-w-[290px] text-[14px] leading-normal">{t('pages.about.presidentPosition')}</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div className="flex items-center justify-between">
-						<div
-							ref={PurposeRef}
-							className="max-w-[721px] translate-y-[50px] opacity-0"
-						>
-							<h2 className="text-text mb-[60px] text-[48px] leading-[1.2] font-bold">{t('pages.about.purpose')}</h2>
-							<p className="text-text text-[16px] leading-normal font-normal">{t('pages.about.text3')}</p>
-						</div>
-
-						<div
-							ref={Image3Ref}
-							className="relative translate-y-[50px] opacity-0"
-						>
-							<div className="relative -translate-y-[230px] transform">
-								<Image
-									src="/imgs/about-img-3.png"
-									alt="About President"
-									width={865}
-									height={414}
-									className="block rounded-[12px]"
-								/>
-
-								<div className="absolute top-0 left-0 flex h-full w-full flex-col items-start justify-center gap-[55px] p-[36px]">
-									<p className="font-regular max-w-[456px] text-left text-[24px] leading-[1.3] text-white">
-										{t('pages.connectionBanner.text')}
-									</p>
-
-									<ButtonTree>{t('commands.join')}</ButtonTree>
-								</div>
-							</div>
-						</div>
+						<h2 className="text-text mb-[60px] text-[48px] leading-[1.2] font-bold">{t('pages.about.purpose')}</h2>
+						<p className="text-text text-[16px] leading-normal font-normal whitespace-pre-line">{t('pages.about.aboutText5')}</p>
 					</div>
 				</Container>
 			</section>
