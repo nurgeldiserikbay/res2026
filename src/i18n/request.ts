415252515042b6169d55d3e8958f88832154d658
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2026-01-05 19:31:48 +0500

    прод подготовить

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,13 @@
-import { hasLocale } from 'next-intl'
+// import { hasLocale } from 'next-intl'
 import { getRequestConfig } from 'next-intl/server'
 
 import { routing } from './routing'
 
-export default getRequestConfig(async ({ requestLocale }) => {
-	const locale = await requestLocale
-	const safeLocale = hasLocale(routing.locales, locale) ? locale : routing.defaultLocale
+// export default getRequestConfig(async ({ requestLocale }) => {
+export default getRequestConfig(async () => {
+	// const locale = await requestLocale
+	// const safeLocale = hasLocale(routing.locales, locale) ? locale : routing.defaultLocale
+	const safeLocale = routing.defaultLocale
 
 	return {
 		locale: safeLocale,
