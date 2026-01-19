d486be59965a277f14e9e7008d4a1fcacc8b7978
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2026-01-19 18:15:35 +0500

    Save

--- src/app/[locale]/about/region/uzbekistan/page.tsx
+++ src/app/[locale]/about/region/uzbekistan/page.tsx
@@ -246,7 +246,7 @@ export default function Page() {
 		<>
 			<HeroBanner
 				title={t(`pages.regions.uzbekistan.title`)}
-				bgImage="/imgs/regions/uz/uzb-banner.png"
+				bgImage="/imgs/regions/uz/uzb-banner.webp"
 				breadcrumbs={[{ label: t(`pages.regions.uzbekistan.title`), href: '/about/region/uzbekistan' }]}
 			/>
 
