220edd1ca1e16e5a15eea68eb46d5164540cf317
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2026-01-22 12:30:19 +0500

    save

--- src/shared/components/BannerSlides.tsx
+++ src/shared/components/BannerSlides.tsx
@@ -11,10 +11,11 @@ import { ButtonDefault } from '@/shared/ui/button/ButtonDefault'
 import { ButtonOutlined } from '@/shared/ui/button/ButtonOutlined'
 import { Container } from '@/shared/ui/container/container'
 
-export function BannerSlides({ images }: { images: { id: number; image: string; description: string }[] }) {
+export function BannerSlides({ images }: { images: { id: number; image: string; description: string; title?: string }[] }) {
 	const [isBeginning, setIsBeginning] = useState(true)
 	const [isEnd, setIsEnd] = useState(false)
 	const [showNavigation, setShowNavigation] = useState(false)
+	const [activeIndex, setActiveIndex] = useState(0)
 
 	const updateShowNavigation = (swiper: { params: { slidesPerView?: number | string } }, count: number) => {
 		const spv = typeof swiper.params.slidesPerView === 'number' ? swiper.params.slidesPerView : 1
@@ -39,16 +40,18 @@ export function BannerSlides({ images }: { images: { id: number; image: string; 
 								setIsBeginning(swiper.isBeginning)
 								setIsEnd(swiper.isEnd)
 								updateShowNavigation(swiper, images.length)
+								setActiveIndex(swiper.activeIndex)
 							}}
 							onSlideChange={(swiper) => {
 								setIsBeginning(swiper.isBeginning)
 								setIsEnd(swiper.isEnd)
+								setActiveIndex(swiper.activeIndex)
 							}}
 						>
 							{images.map((slide) => (
 								<SwiperSlide
 									key={slide.id}
-									className="h-[287px] overflow-hidden rounded-[12px] md:h-[888px]"
+									className="relative h-[287px] overflow-hidden rounded-[12px] md:h-[888px]"
 								>
 									<Image
 										src={slide.image}
@@ -64,8 +67,17 @@ export function BannerSlides({ images }: { images: { id: number; image: string; 
 						</Swiper>
 					</div>
 					<div className="relative bottom-0 left-0 z-5 flex flex-col items-start justify-end md:absolute md:px-[25px] md:py-[25px] lg:px-[50px] lg:py-[50px]">
+						<div className="pointer-events-none">
+							{images?.length > 0 && images[activeIndex]?.title && (
+								<div className="">
+									<h3 className="text-[24px] leading-normal font-bold text-text md:text-[40px] md:text-white 2xl:text-[48px]">
+										{images[activeIndex].title}
+									</h3>
+								</div>
+							)}
+						</div>
 						<div
-							className={`flex items-center justify-start gap-[20px] md:mt-[136px] ${!showNavigation ? 'hidden' : ''}`}
+							className={`mt-[30px] flex items-center justify-start gap-[20px] md:mt-[46px] ${!showNavigation ? 'hidden' : ''}`}
 							aria-hidden={!showNavigation}
 						>
 							<div className="flex items-center justify-start gap-[10px]">
@@ -73,9 +85,9 @@ export function BannerSlides({ images }: { images: { id: number; image: string; 
 									{isBeginning ? (
 										<ButtonOutlined
 											icon={false}
-											className="text-muted pointer-events-none box-border h-[45px] w-[82px] cursor-default rounded-[8px]! p-[8px]!"
+											className="pointer-events-none box-border h-[45px] w-[82px] cursor-default rounded-[8px]! p-[8px]! text-muted"
 										>
-											<IconArrowHead className="text-muted rotate-180 transform" />
+											<IconArrowHead className="rotate-180 transform text-muted" />
 										</ButtonOutlined>
 									) : (
 										<ButtonDefault
@@ -91,7 +103,7 @@ export function BannerSlides({ images }: { images: { id: number; image: string; 
 									{isEnd ? (
 										<ButtonOutlined
 											icon={false}
-											className="text-muted pointer-events-none box-border h-[45px] w-[82px] cursor-default rounded-[8px]! p-[8px]!"
+											className="pointer-events-none box-border h-[45px] w-[82px] cursor-default rounded-[8px]! p-[8px]! text-muted"
 										>
 											<IconArrowHead className="text-muted" />
 										</ButtonOutlined>
--- src/widgets/useful-astana/ui/AstanaSlides.tsx
+++ src/widgets/useful-astana/ui/AstanaSlides.tsx
@@ -1,55 +1,111 @@
 'use client'
 
+import { useLocale } from 'next-intl'
+
 import { BannerSlides } from '@/shared/components/BannerSlides'
+import { Locale } from '@/shared/config/i18n'
 
 export function AstanaSlides() {
+	const locale = useLocale() as Locale
 	const images = [
 		{
 			id: 8,
 			image: '/imgs/usefulls/astana-slide-9.webp',
+			title: {
+				ru: 'Байтерек',
+				kk: 'Байтерек',
+				en: 'Bayterek',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 0,
 			image: '/imgs/usefulls/astana-slide-1.webp',
+			title: {
+				ru: 'Театр имени Розы Баглановой',
+				kk: 'Роза Бағланова атындағы театр',
+				en: 'Roza Baglanova Theatre',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 1,
 			image: '/imgs/usefulls/astana-slide-2.webp',
+			title: {
+				ru: 'EXPO 2017',
+				kk: 'EXPO 2017',
+				en: 'EXPO 2017',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 2,
 			image: '/imgs/usefulls/astana-slide-3.webp',
+			title: {
+				ru: 'Мост на набережной',
+				kk: 'Жағалаудағы көпір',
+				en: 'Bridge on the Embankment',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 3,
 			image: '/imgs/usefulls/astana-slide-4.webp',
+			title: {
+				ru: 'Астана Опера',
+				kk: 'Астана Опера',
+				en: 'Astana Opera',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 4,
 			image: '/imgs/usefulls/astana-slide-5.webp',
+			title: {
+				ru: 'Мангилик Ел',
+				kk: 'Мәңгілік Ел',
+				en: 'Mangilik El',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 5,
 			image: '/imgs/usefulls/astana-slide-6.webp',
+			title: {
+				ru: 'Мост Сарыарка',
+				kk: 'Сарыарқа көпірі',
+				en: 'Saryarka Bridge',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 6,
 			image: '/imgs/usefulls/astana-slide-7.webp',
+			title: {
+				ru: 'Ак Орда',
+				kk: 'Ақ Орда',
+				en: 'Ak Orda',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 		{
 			id: 7,
 			image: '/imgs/usefulls/astana-slide-8.webp',
+			title: {
+				ru: 'Республиканская Главная Мечеть',
+				kk: 'Республикалық Бас Мешіт',
+				en: 'Republican Central Mosque',
+			},
 			description: 'Astana is the capital of Kazakhstan',
 		},
 	]
 
-	return <BannerSlides images={images} />
+	const parsedImages = images.map((image) => {
+		return {
+			...image,
+			title: image.title[locale as Locale],
+		}
+	})
+
+	return <BannerSlides images={parsedImages} />
 }
